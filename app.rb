8c13ed91a229988e2d4d8695d283010473c89632
Ming Duan <11203duan@gmail.com>
2019-05-16 21:30:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My name is Jon"
+    "My name is Jon."
   end
 
   get '/hometown' do
-    "My hometown is Winterfell"
+    "My hometown is Winterfell."
   end
 
   get '/favorite-song' do
-    "Favorite song"
+    "My favorite song is Poker Face"
   end
 
 end
