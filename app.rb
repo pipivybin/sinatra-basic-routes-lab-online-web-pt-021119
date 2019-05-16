1ed37b903e29f4b98248e57fed1a6d590e265d40
Ming Duan <11203duan@gmail.com>
2019-05-16 21:31:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jon."
+  end
+
+  get '/hometown' do
+    "My hometown is Winterfell."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Love Story."
+  end
+
 end
